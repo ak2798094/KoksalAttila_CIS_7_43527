3aecf341b53e440b095b917a715cc31f5a5fedb0
Attila Koksal <akoksal@student.rccd.edu>
2022-03-15 14:46:11 -0700

    Signed-off-by: Attila Koksal <akoksal@student.rccd.edu>

--- Lab/Lab_Luhn_Algorithm_2/main.cpp
+++ Lab/Lab_Luhn_Algorithm_2/main.cpp
@@ -1,199 +1,212 @@
 /* 
  * File:   main.cpp
  * Author: Attila Koksal
- * Purpose:  Lab Luhn Algorithm 2
+ * Purpose: Lab Luhn Algorithm 2
+ * Created on March 15, 2022, 2:40 PM
  */
 
+//System Libraries - Post Here
 //System Libraries
 #include <iostream>  //I/O Library
 #include <cstdlib>   //Random Function Library
-#include <iomanip>
 #include <ctime>     //Time Library
+#include <cstring>
 using namespace std;
 
 //Global Constants
-enum cardLst {AmEx, Visa, MstrCrd, Disc, All};
+//Enum       15   34 or 37   16 4  16 5        16 6011
+enum CrdCard{AmericanExpress,Visa,MasterCard,Discover,ALL}; // make an if statement if ALL is picked set to randomly pick the other for cards
+int valid=0, invalid=0;
 
 //Function Prototypes
-char rndDgit();          //Randomly generates digit characters
-char *prpLuhn(int,int);       //Prepares the Luhn Sequence
+char rndDgit();           //Randomly generates digit characters
+void prpLuhn(char[],int); //Prepares the Luhn Sequence
 int  cnvrt(char);         //Convert 1 digit char to integer
 char cnvrt(int);          //Convert 1 digit integer to a char
 void output(char[],int);  //Output the sequence as integers
 //Function you are to create
-int *Luhn(char*,int);    //Calculate and append the Luhn Digit
-int findNum(int*, int);
-bool check(char*, char*, int); //Check validity
+bool Luhn(char[],int,int);    //Calculate and append the Luhn Digit
+void genCC(CrdCard, char[],int);
+void flipDig(char[]);
+bool validCC(char[]);
+
 
 int main(int argc, char** argv) {
+    
     //Set the random number seed
     srand(static_cast<unsigned int>(time(0)));
-    //Declare int value for number of valid and invalid cards
-    int valid = 0, invalid = 0;
-    //Initialize values
-    int cardType = 0;
-    int SIZE = 0;
-    int finalNum = 0;
-    int randInd = 0;
-    //Make it a loop
-    for(int i = 0; i < 10000; i++) {
-        //Create function to make valid card depending on what type of card it is.
-        //choose which type of card
-        cardType = rand()%5;
-        //Set values for card
-        char *crdCard = new char;
-        if(cardType == AmEx) {
-            cout << "American Express Card:" << endl << endl;
-            //One more than actual size to account for null
-            SIZE = 16;
+    
+    //Set the Credit Card size
+    int SIZE=17;
+    char crdCard[SIZE];
+    CrdCard CC;
+    int num; //counter
+    
+    for(int i=0; i<10000; i++){
+        SIZE=17;
+        num=rand()%4;
+        if(num==0){
+            CC=AmericanExpress;
+            SIZE=16;
         }
-        else if(cardType == Visa) {
-            cout << "Visa Card:" << endl << endl;
-            //One more than actual size to account for null
-            SIZE = 17;
+        else if(num==1){
+            CC=Visa;
         }
-        else if(cardType == MstrCrd) {
-            cout << "Master Card:" << endl << endl;
-            //One more than actual size to account for null
-            SIZE = 17;
+        else if (num==2){
+            CC=MasterCard;
         }
-        else if(cardType == Disc) {
-            cout << "Discover Card:" << endl << endl;
-            //One more than actual size to account for null
-            SIZE = 20;
+        else if(num==3){
+            CC=Discover;
         }
-        else if(cardType == All) {
-            cout << "Random Card:" << endl << endl;
-            //One more than actual size to account for null
-            SIZE = rand() % (21 - 16) + 16;
+        
+        if(CC==ALL){
+            num=rand()%3;
+            if(num==0){
+                CC=AmericanExpress;
+                SIZE=16;
+            }
+            else if(num==1){
+                CC=Visa;
+            }
+            else if (num==2){
+                CC=MasterCard;
+            }
         }
-        //Create Int array to add numbers
-        int *cardInt = new int(SIZE - 2);
-        //Create card Numbers
-        crdCard = prpLuhn(SIZE-2,cardType);
-        //using the Luhn Algorithm
-        cardInt = Luhn(crdCard,SIZE);
-        //Find luhn num
-        finalNum = findNum(cardInt,SIZE);
-        //Set last num to luhn
-        crdCard[SIZE - 2] = finalNum + 48;
-        //Output
-        cout << "Valid card number:" << endl;
-        //Output valid card
-        cout << crdCard << endl << endl;
-        //Random for flipp number
-        randInd = rand()%SIZE;
-        //Allocate memory and initialize
-        char *flip = new char(SIZE);
-        for(int i = 0; i < SIZE - 1; i++) {
-            flip[i] = crdCard[i];
-        }
-        //Flip a random number
-        flip[randInd] = rand()%10 + 48;
-        //Output random number flipped
-        cout << "Random Number Flipped:" << endl;
-        for(int i = 0; i < SIZE - 1; i++) {
-            cout << flip[i];
-        }
-        cout << endl << endl;
-        //count valid and invalid flips
-        if(check(crdCard,flip,SIZE)) {
-            cout << "Valid" << endl << endl;
-            valid++;
-        }
-        else {
-            cout << "Invalid" << endl << endl;
-            invalid++;
-        }
-        //De allocate
-        delete [] crdCard;
-        delete  cardInt;
-        delete [] flip;
+        
+        genCC(CC, crdCard, SIZE-2);
+        
     }
-    //Percentage
-    float perc = 0;
-    perc = valid/100;
-    //Output results
-    cout << "Number of Valid Numbers  : " << valid << endl
-         << "Number of Invalid Numbers: " << invalid << endl << endl;
-    cout << setprecision(2) << "Percent of validity      : " << perc << "%" << endl;
-    //Exit Stage Right
+    cout<<"Amount of Valid Credit Cards: "<<valid<<endl;
+    cout<<"Amount of Invalid Credit Cards: "<<invalid<<endl;
+
     return 0;
 }
 
-bool check(char *a, char *b, int SIZE) {
-    bool validity = true;
-    //Check validity
-    for(int i = 0; i < SIZE - 1; i++) {
-        if(a[i] == b[i]) {
-            validity = true;
-        }
-        else {
-            validity = false;
-            return validity;
-        }
+// GENERATE CC HERE
+void genCC(CrdCard CC, char cc[], int n){
+    
+    for(int i=0;i<n+1;i++){
+        cc[i]=rndDgit();
     }
-    //Return
-    return validity;
-}
+    int last=cnvrt(cc[n]); // holds last digit in array
+ 
+    for(int i=n;i<=n+1;i++){
+        cc[i]='\0';
+    }
+    
+    Luhn(cc, n, last);
+    flipDig(cc);
 
-int findNum(int *cardInt, int SIZE) {
+    if(Luhn(cc, n, last)==true){
+        validCC(cc);
+    }
+    else{
+        invalid++;
+    }
+}
+void flipDig(char cc[]){
+    int length=strlen(cc);
+    int num, num2, add;
+        
+    if(length==15){
+        num=rand()%15;
+        num2=cnvrt(cc[num]);
+        add=num2+1;
+        if(add>=9){
+            add=9;
+        }
+        cc[num]=cnvrt(add);
+    }
+    else if(length==14){
+        num=rand()%14;
+        num2=cnvrt(cc[num]);
+        add=num2+1;   
+        if(add>=9){
+            add=9;
+        }
+        cc[num]=cnvrt(add);
+//        cout<<"New num"<<cc[num]<<endl;;
+    }
+//        cout<<"Rand flipped dig "<<cc[num]<<endl;
+
+}
+bool validCC(char cc[]){   
+        if(cc[0]==6 && cc[1]==0 && cc[2]==1 && cc[3]==1){
+            valid++;
+        }
+        else if(cc[0]=3 && (cc[1]==4 || cc[1]==7)){
+            valid++;
+        }
+        else if(cc[0]=4){
+            valid++;
+        }
+        else if(cc[0]=5){
+            valid++;
+        }
+    
+    return valid;
+}
+bool Luhn(char ccc[],int n,int last){
+    int checkSum;
+        
+    // 1st step is done in the prepLun function
+    
+//    // 2nd step
+    // Use a for loop used to swap each element in the array
+    char temp;
+    for(int i=0, j=n-1; i<=6; i++,j--){ 
+        temp=ccc[i];
+        ccc[i]=ccc[j];
+        ccc[j]=temp;
+//        cout<<ccc[i];
+    }
+
+    // 3rd step
+    // multiply every odd element and subtract 9 to all any result higher than 9
     int num;
-    //Add all numbers together
-    for(int i = 0; i < SIZE - 2; i++){
-        //Subtract 9 if its over 10
-        if(cardInt[i] >= 10) {
-            cardInt[i] -= 9;
-            num += cardInt[i];
-        }
-        else {
-            num += cardInt[i];
-        }
+    int sum=0;
+    
+    for(int i=0; i<=n-1 ;i++){
+        
+        num=cnvrt(ccc[i]);
+       
+        
+        if(i%2==0){
+            num=num*2;
+            if(num>9){
+                num=num-9;
+                ccc[i]=cnvrt(num);
+            }
+            ccc[i]=cnvrt(num);
+        }       
+        
+        ccc[i]=cnvrt(num);
+//        cout<<ccc[i]<<endl;
+        sum+=num;
     }
-    //take modulus of 10
-    num = num%10;
-    //Return number
-    return num;
+//    cout<<endl<<"Luhn Output step 3"<<endl;
+//    cout<<ccc<<endl;
+    
+    //  last step   
+    checkSum=sum%10;
+    // put the last steps here
+//    int count=0;
+    bool credit=false;
+    if(checkSum==last){
+        credit==true;
+        return true;
+    }
+    return false;
+//    cout<<count<<endl; 
+    
 }
-
-int *Luhn(char *crdCard, int SIZE){
-    //allocate memory
-    int *cardInt = new int(SIZE);
-    int placeholder = 0;
-    //Initialize all to 0
-    for(int i = 0; i < SIZE; i++) {
-        cardInt[i] = 0;
-    }
-    //Loop to get int array reversed and doubled
-    for(int i = SIZE - 3; i >= 0; i--) {
-        //Set placeholder to equal to number value
-        placeholder = cnvrt(crdCard[i]);
-        //Double number if its odd;
-        //Boolean statement is for evens as number count starts at 0
-        if(i%2 == 0) {
-            //Multiply Placeholder value by 2
-            placeholder = placeholder*2;
-            //Set i value equal
-            cardInt[i] = placeholder;
-        }
-        else {
-            cardInt[i] = placeholder;
-        }
-    }
-    //Return Integer array
-    return cardInt;
-    //De allocate
-    delete []cardInt;
-}
-
 void output(char cc[],int n){
-    cout << "The char array output as each integer digit" << endl;
-    for(int i = 0; i < n; i++){
-        cout << cnvrt(cc[i]);
+    cout<<"The char array output as each integer digit"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<cnvrt(cc[i]);
     }
-    cout << endl << endl;
+    cout<<endl;
 }
-
 char  cnvrt(int digit){
     if(digit<0||digit>9){
         cout<<"You don't know what you are doing"<<endl;
@@ -202,7 +215,6 @@ char  cnvrt(int digit){
     return digit+'0';
     return digit+48;
 }
-
 int  cnvrt(char digit){
     if(digit<'0'||digit>'9'){
         cout<<"You don't know what you are doing"<<endl;
@@ -210,40 +222,17 @@ int  cnvrt(char digit){
     }
     return digit-'0';
 }
-
-char *prpLuhn(int n, int cardType){
-    //allocate memory
-    char *cc = new char(n);
-    //Change first number depending on what type
-    if(cardType == AmEx) {
-        cc[0] = 3 + 48;
-    }
-    else if(cardType == Visa) {
-        cc[0] = 4 + 48;
-    }
-    else if(cardType == MstrCrd) {
-        cc[0] = 5 + 48;
-    }
-    else if(cardType == Disc) {
-        cc[0] = 6 + 48;
-    }
-    else if(cardType == All) {
-        cc[0] = (rand() % (7 - 4) + 4) + 48;
-    }
+void prpLuhn(char cc[],int n){
     //Create a random cc in prep for Luhn checksum
-    for(int i = 1; i < n; i++){
-        cc[i] = rndDgit();
+    for(int i=0;i<n;i++){
+        cc[i]=rndDgit();
     }
-    //Put null terminator at the end
-    for(int i = n; i <= n + 1; i++){
-        cc[i] = '\0';
-    }
-    //Return array
-    return cc;
-    //Deallocate
-    delete []cc;
-}
 
+    //Put null terminator at the end
+    for(int i=n;i<=n+1;i++){
+        cc[i]='\0';
+    }
+}
 char rndDgit(){
     return rand()%10+'0';//Ascii Code for numbers '0'=48 and '9'=57
     //return rand()%10+48;//Ascii Code for numbers '0'=48 and '9'=57
