23fce92869682ca685cb8c1d024c309297e54a45
Attila Koksal <akoksal@student.rccd.edu>
2022-05-18 10:08:03 -0700

    Signed-off-by: Attila Koksal <akoksal@student.rccd.edu>

---
+++ Lab/LabBloomFilter/RunnerProgram/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Attila Koksal
+ * Created on May 17, 2022, 5:35 PM
+ * Purpose:  Demonstrate Bloom Filter
+ */
+
+//System Level Libraries
+#include <iostream>
+#include <string>
+#include <bitset>
+
+#include "GeneralHashFunctions.h"
+#include "BloomFilter.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    
+   // Seed random
+    srand(time(0));
+    
+    // Run the problems
+    problemOne();
+    problemsTwoAndThree();
+    return 0;
+   
+}
