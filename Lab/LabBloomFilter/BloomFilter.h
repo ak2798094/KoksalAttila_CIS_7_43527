c480ef34b10817949b11c2c479e2a8f71d9b2daa
Attila Koksal <akoksal@student.rccd.edu>
2022-05-18 09:34:44 -0700

    Signed-off-by: Attila Koksal <akoksal@student.rccd.edu>

---
+++ Lab/LabBloomFilter/BloomFilter.h
@@ -0,0 +1,19 @@
+#ifndef BLOOMFILTER_H
+#define BLOOMFILTER_H
+
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+#include "GeneralHashFunctions.h"
+
+using namespace std;
+
+void displayProblem(int problem);
+
+void problemOne();
+void problemsTwoAndThree();
+
+#endif /* BLOOMFILTER_H */
+
